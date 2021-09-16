20ef1f2d597a392202bac4d3a60dc0160013263b
tmstudenler <tmstudentler@gmail.com>
2021-09-16 15:50:21 +0300

    step 3

---
+++ app/src/main/java/com/example/videomanager/model/LoginCall.java
@@ -0,0 +1,40 @@
+package com.example.videomanager.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class LoginCall {
+    @SerializedName("registration_token")
+    @Expose
+    private String registrationToken;
+    @SerializedName("manager_id")
+    @Expose
+    private Integer managerId;
+    @SerializedName("type")
+    @Expose
+    private String type;
+
+    public String getRegistrationToken() {
+        return registrationToken;
+    }
+
+    public void setRegistrationToken(String registrationToken) {
+        this.registrationToken = registrationToken;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
